adcd78515ccd5c8d8f61b9cd28c51a0fcb8a923d
Mircea Nistor <mirceanis@gmail.com>
2019-05-06 14:50:26 +0300

    add simple test to demoapp just to check compilation

---
+++ demoapp/src/test/java/me/uport/mnid/demo/MnidActivityTest.kt
@@ -0,0 +1,13 @@
+package me.uport.mnid.demo
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class MnidActivityTest {
+
+    @Test
+    fun `can run test`() {
+        assertTrue(true)
+    }
+
+}
