b0d1c078796d91eceabe4ef87319f30cac5119a0
Mircea Nistor <mirceanis@gmail.com>
2020-03-20 17:12:27 +0100

    fix: use implementation for deps to try to add them to pom

--- demoapp/src/test/java/me/uport/mnid/demo/MnidActivityTest.kt
+++ demoapp/src/test/java/me/uport/mnid/demo/MnidActivityTest.kt
@@ -1,5 +1,7 @@
 package me.uport.mnid.demo
 
+import me.uport.mnid.Account
+import org.junit.Assert
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -8,6 +10,13 @@ class MnidActivityTest {
     @Test
     fun `can run test`() {
         assertTrue(true)
+    }
+
+    @Test
+    fun `can use library`() {
+        val expected = Account.from("0x01", "0x0000000000000000000000000000000000001234")
+        val acc = Account.from("2nQs23uc3UN6BBPqGHpbudDxBkeNVX7YRBb")
+        Assert.assertEquals(expected, acc)
     }
 
 }
